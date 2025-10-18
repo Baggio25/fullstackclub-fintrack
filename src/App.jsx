703c2d5df82661be5e315ddb2b074f2d46c67da8
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-10-17 21:58:00 -0300

    chore: add shadcn setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
-  return <h1 className="text-red-500">Fintrack</h1>;
+  return (
+    <div>
+      <h1 className="text-red-500">Fintrack</h1>
+      <Button>Fazer login</Button>
+    </div>
+  );
 };
 
 export default App;
